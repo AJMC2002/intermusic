77252c84d24581bc10f1b9bbc08d854535113c6b
AJMC2002 <adrianmosquera2015@gmail.com>
2023-01-30 22:49:40 +1000

    gitignore update & featured artists formatting added

---
+++ server/src/utils/formatGeniusArtistArray.ts
@@ -0,0 +1,12 @@
+import { Artist, GeniusArtist } from "../types";
+import formatGeniusArtist from "./formatGeniusArtist";
+
+export const formatGeniusArtistArray = function (
+	artists: GeniusArtist[]
+): Artist[] {
+	return artists.map(formatGeniusArtist).sort((a, b) => {
+		return a.name.localeCompare(b.name);
+	});
+};
+
+export default formatGeniusArtistArray;
--- server/src/utils/formatGeniusSong.ts
+++ server/src/utils/formatGeniusSong.ts
@@ -1,6 +1,7 @@
 import { GeniusSong, Song } from "../types";
 import formatGeniusAlbum from "./formatGeniusAlbum";
 import formatGeniusArtist from "./formatGeniusArtist";
+import formatGeniusArtistArray from "./formatGeniusArtistArray";
 import getLyrics from "./getLyrics";
 import getYouTubeURL from "./getYouTubeURL";
 
@@ -11,7 +12,7 @@ export const formatGeniusSong = async function (
 		id: song.id,
 		name: song.title,
 		artist: formatGeniusArtist(song.primary_artist),
-		featured_artists: song.featured_artists.map(formatGeniusArtist),
+		featured_artists: formatGeniusArtistArray(song.featured_artists),
 		album: formatGeniusAlbum(song.album),
 		release_date: song.release_date,
 		language: song.language,
